739c951dabaccc63b68d8db30352b44194613561
William Deegan <bill@baddogconsulting.com>
2022-03-09 20:06:23 -0800

    example from scons-contrib issue #45

---
+++ scons-contrib/45/example2/hello.cpp
@@ -0,0 +1,12 @@
+#include <QtGui/qapplication.h>
+#include <QtGui/qlabel.h>
+
+#include "hello.h"
+
+class HelloLabel : public QLabel {
+  Q_OBJECT
+};
+
+int main() {
+  return 0;
+}
---
+++ scons-contrib/45/example2/hello.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <QtGui/qapplication.h>
+#include <QtGui/qlabel.h>
+
+class HelloLabel2 : public QLabel {
+  Q_OBJECT
+};
